63e387eebbbe76358c29ef44327a9f4c4fe5b414
Kuba429 <jakubzawadzki429@gmail.com>
2022-06-14 14:34:56 +0200

    type fix

--- src/pages/books.tsx
+++ src/pages/books.tsx
@@ -1,4 +1,10 @@
-import { useContext, useEffect, useState } from "react";
+import {
+    Dispatch,
+    SetStateAction,
+    useContext,
+    useEffect,
+    useState,
+} from "react";
 import Layout from "../components/Layout";
 import { supabaseClient } from "../utils/supabaseClient";
 import { book } from "../interfaces";
@@ -29,14 +35,14 @@ export default function Books() {
     );
 }
 // i'm aware that passing setState as a prop outside component may not be the safest way to do this. In this case i don't really see any drawbacks. Other solution would be to create a function inside the component that invokes the function below and sets the state to it's result.
-const getBooks = async (setBooks: any) => {
+const getBooks = async (setBooks: Dispatch<SetStateAction<Array<book>>>) => {
     try {
         const res = await supabaseClient
             .from("books")
             .select("id,title,author,pages,language");
 
         if (res.error) throw new Error(res.error.message);
-        setBooks(res.data!);
+        setBooks(res.data);
     } catch (error) {
         console.log(error);
     }
