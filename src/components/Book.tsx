bd26a6567648feaeea17dec895cf1e4f20a74c68
Kuba429 <jakubzawadzki429@gmail.com>
2022-06-15 14:12:48 +0200

    prevent user from adding the same book twice

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -1,17 +1,7 @@
 import React from "react";
 import { book as bookInterface } from "../interfaces";
 import { supabaseClient } from "../utils/supabaseClient";
-const addBook = async (bookId: string, userId: string) => {
-    if (!userId) {
-        return;
-    }
-    const res = await supabaseClient.from("books_read").insert([
-        {
-            user_id: userId,
-            book_id: bookId,
-        },
-    ]);
-};
+
 const Book: React.FC<{ book: bookInterface; userId: string }> = ({
     book,
     userId,
@@ -30,3 +20,28 @@ const Book: React.FC<{ book: bookInterface; userId: string }> = ({
     );
 };
 export default Book;
+
+const addBook = async (bookId: string, userId: string) => {
+    if (!userId) {
+        return;
+    }
+    // check if book was already added to user's list
+    try {
+        const resCheck = await supabaseClient
+            .from("books_read")
+            .select("id")
+            .eq("book_id", bookId)
+            .eq("user_id", userId);
+        if (resCheck.data?.length! > 0)
+            throw new Error("This book is already in your list");
+        const res = await supabaseClient.from("books_read").insert([
+            {
+                user_id: userId,
+                book_id: bookId,
+            },
+        ]);
+        if (res.error) throw new Error(res.error.message);
+    } catch (error) {
+        console.error(error);
+    }
+};
