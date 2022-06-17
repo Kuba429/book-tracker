91098d88f4615bc22d44963ac6dc7476dc4cbc9e
Kuba429 <jakubzawadzki429@gmail.com>
2022-06-17 13:52:37 +0200

    hide menu on click outside

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,7 +21,7 @@ const Navbar: React.FC<{ userMail: string }> = ({ userMail }) => {
                 <button onClick={() => setIsVisible((x) => !x)}>toggle</button>
             </div>
             <div
-                className={`w-80 h-full top-0 overflow-y-auto text-white bg-slate-800 flex flex-col items-start gap-4 justify-start py-10 px-10 transition-all
+                className={`w-80 h-full top-0 overflow-y-auto text-white bg-slate-800 flex flex-col items-start gap-4 justify-start py-10 px-10 transition-all z-20
                 absolute md:static 
                 ${!isVisible && "-translate-x-full"} 
                 md:translate-x-0
@@ -48,6 +48,15 @@ const Navbar: React.FC<{ userMail: string }> = ({ userMail }) => {
                 <span>{userMail}</span>
                 <button onClick={handleSignOut}>Sing out</button>
             </div>
+            {/* Make space outside toggled nav menu hide the menu on click */}
+            {isVisible && (
+                <div
+                    onClick={() => {
+                        setIsVisible((x) => !x);
+                    }}
+                    className="bg-black opacity-50 z-10 absolute top-0 w-full h-full"
+                ></div>
+            )}
         </>
     );
 };
