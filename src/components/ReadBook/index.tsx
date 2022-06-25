798962cfd60a1a1dc4ff5d23c3c4a92d5477c4fd
Kuba429 <jakubzawadzki429@gmail.com>
2022-06-25 10:06:15 +0200

    unused import

--- src/components/ReadBook/index.tsx
+++ src/components/ReadBook/index.tsx
@@ -1,4 +1,3 @@
-import { type } from "os";
 import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { readBook, readBook as readBookInterface } from "../../interfaces";
 import defaultCover from "../../utils/defaultCover";
