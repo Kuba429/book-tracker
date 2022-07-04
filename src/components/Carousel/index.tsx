9f5a3c50d4aa1e25bd06f91f0b58b764ec337e55
Kuba429 <jakubzawadzki429@gmail.com>
2022-07-04 13:34:30 +0200

    carousel styling

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,3 +1,5 @@
+import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import defaultCover from "../../utils/defaultCover";
 import { usePage } from "../../utils/hooks/usePage";
@@ -5,6 +7,7 @@ interface Tile {
 	url: string;
 	id: number;
 }
+const TILE_HEIGHT = "h-36"; // set here to make buttons the same height
 const Carousel = () => {
 	let data: Array<Tile> = [];
 	for (let i = 0; i < 20; i++) data.push({ url: defaultCover!, id: i });
@@ -12,30 +15,36 @@ const Carousel = () => {
 	const [page, addPage, subtractPage] = usePage(data.length);
 	return (
 		<div className="carousel">
-			<button onClick={subtractPage} className="buttonLeft">
-				⬅
+			<button
+				onClick={subtractPage}
+				className={`buttonLeft ${TILE_HEIGHT} w-6`}
+			>
+				<FontAwesomeIcon icon={faAngleLeft} />
 			</button>
 			<div
 				className="track"
 				style={{ transform: `translateX(${page * -100}%)` }}
 			>
 				{data.map((x) => (
-					<Tile data={x} key={x.id} page={page} />
+					<Tile data={x} key={x.id} />
 				))}
 			</div>
-			<button onClick={addPage} className="buttonRight">
-				➡
+			<button
+				onClick={addPage}
+				className={`buttonRight ${TILE_HEIGHT} w-6`}
+			>
+				<FontAwesomeIcon icon={faAngleRight} />
 			</button>
 		</div>
 	);
 };
 export default Carousel;
 
-const Tile: React.FC<{ data: Tile; page: number }> = ({ data, page }) => {
+const Tile: React.FC<{ data: Tile }> = ({ data }) => {
 	return (
 		<div className="tile">
 			<img
-				className="object-contain m-auto w-24 h-36"
+				className={`object-contain m-auto w-24 ${TILE_HEIGHT} rounded`}
 				src={data.url}
 				alt={data.id as unknown as string}
 			/>
