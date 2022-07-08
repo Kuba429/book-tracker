7498280f689c5b3ab7d0b44ab7a70ca48bd89513
Kuba429 <jakubzawadzki429@gmail.com>
2022-07-08 20:25:26 +0200

    buttons styling

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -27,14 +27,18 @@ const Carousel: FC<{ data: Array<Tile>; header?: string }> = ({
 				>
 					<FontAwesomeIcon icon={faAngleLeft} />
 				</button>
-				<div
-					className="track"
-					style={{ transform: `translateX(${page * -100}%)` }}
-				>
-					{data.map((x) => (
-						<Tile data={x} key={x.uuid} />
-					))}
+				<div className="overflow-hidden">
+					{/* wrapper to hide overflow of track but not buttons so they can be moved; buttons have rounded corners which makes book covers from other slides visible; this allows me to translate the buttons so that doesn't happen; giving overflow-hidden to parent of this div would also hide overflown buttons */}
+					<div
+						className="track"
+						style={{ transform: `translateX(${page * -100}%)` }}
+					>
+						{data.map((x) => (
+							<Tile data={x} key={x.uuid} />
+						))}
+					</div>
 				</div>
+
 				<button
 					onClick={addPage}
 					className={`buttonRight ${TILE_HEIGHT} w-6`}
@@ -51,7 +55,7 @@ const Tile: React.FC<{ data: Tile }> = ({ data }) => {
 	return (
 		<div className="tile">
 			<img
-				className={`object-contain m-auto w-24 ${TILE_HEIGHT} rounded`}
+				className={`object-contain ${TILE_HEIGHT} rounded`}
 				src={data.imgUrl}
 				alt={data.id as unknown as string}
 			/>
