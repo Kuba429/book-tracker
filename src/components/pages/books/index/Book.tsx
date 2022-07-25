92e6e5144cdcda8962479a05a9348c4d083b228f
Kuba429 <jakubzawadzki429@gmail.com>
2022-07-25 18:23:00 +0200

    disabled buttons for already added books

--- src/components/pages/books/index/Book.tsx
+++ src/components/pages/books/index/Book.tsx
@@ -1,14 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { book as bookInterface } from "interfaces";
 import defaultCover from "supabase/defaultCover";
 import { supabaseClient } from "supabase/client";
 import { AddButton } from "components/shared/AddButton";
 import Link from "next/link";
+import { UserContext } from "components/Layout/ContextWrapper";
 
 const Book: React.FC<{ book: bookInterface; userId: string }> = ({
 	book,
 	userId,
 }) => {
+	const context = useContext(UserContext);
 	const [coverUrl, setCoverUrl] = useState("");
 	useEffect(() => {
 		// fetching book cover url; falling back to the default one, fetched ahead of time to avoid unnecessary requests
@@ -21,6 +23,9 @@ const Book: React.FC<{ book: bookInterface; userId: string }> = ({
 			setCoverUrl(data || defaultCover!); // fall back to default cover if needed
 		}
 	}, [book]);
+	useEffect(() => {
+		if (context?.addedBooksIDs == null) context?.refetchReadIDs();
+	}, []);
 	return (
 		<Link href={`/book/${book.id}`} key={book.id}>
 			<a className="book-card">
@@ -33,7 +38,18 @@ const Book: React.FC<{ book: bookInterface; userId: string }> = ({
 					<p>{book.title}</p>
 					<p>{book.author}</p>
 					<p>{book.pages} pages</p>
-					<AddButton book={book} userId={userId} />
+					{context?.addedBooksIDs?.includes(book.id.toString()) ? (
+						<button
+							disabled
+							className="btn group disabled:after:bg-gray-400"
+						>
+							<span className="group-disabled:bg-gray-300 group-disabled:text-gray-700">
+								Added
+							</span>
+						</button>
+					) : (
+						<AddButton book={book} userId={userId} />
+					)}
 				</div>
 			</a>
 		</Link>
